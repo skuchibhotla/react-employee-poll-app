afc54ecd0dd84c2b57aa77f3e07dfb5fd88ab63b
Shashank Kuchibhotla <sk.kuchibhotla@gmail.com>
2024-04-02 21:08:08 -0600

    Fixing 404 URL

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App({dispatch, loggedIn}) {
                 <Route path="/leaderboard" exact element={<PrivateRoute><Leaderboard/></PrivateRoute>}/>
                 <Route path="/questions/:id" element={<PrivateRoute><PollPage/></PrivateRoute>}/>
                 <Route path="/new" exact element={<PrivateRoute><NewPoll/></PrivateRoute>}/>
-                <Route path="/error" exact element={<Error/>}/>
+                <Route path="/404" exact element={<Error/>}/>
             </Routes>
         </div>
     );
